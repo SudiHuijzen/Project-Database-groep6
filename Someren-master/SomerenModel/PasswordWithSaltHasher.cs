67c946fb4cf79769bf98fe29df6f7c8b72f0863d
SudiHuijzen <sudi.huijzen@gmail.com>
2022-03-29 21:07:57 +0200

    Opdracht 5
    geen id

---
+++ Someren-master/SomerenModel/PasswordWithSaltHasher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class PasswordWithSaltHasher
+    {
+        public static HashSalt HashWithSalt(int size, string password)
+        {/*
+            RNG rng = new RNG();
+            byte[] saltBytes = rng.GenerateRandomCryptographicBytes(saltLength);
+            byte[] passwordAsBytes = Encoding.UTF8.GetBytes(password);
+            List<byte> passwordWithSaltBytes = new List<byte>();
+            passwordWithSaltBytes.AddRange(passwordAsBytes);
+            passwordWithSaltBytes.AddRange(saltBytes);
+            byte[] digestBytes = hashAlgo.ComputeHash(passwordWithSaltBytes.ToArray());
+            return new HashWithSaltResult(Convert.ToBase64String(saltBytes), Convert.ToBase64String(digestBytes));*/
+            var saltBytes = new byte[size];
+            var provider = new RNGCryptoServiceProvider();
+            provider.GetNonZeroBytes(saltBytes);
+            var salt = Convert.ToBase64String(saltBytes);
+
+            var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, saltBytes, 10000);
+            var hashPassword = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256));
+
+            HashSalt hashSalt = new HashSalt { Hash = hashPassword, Salt = salt };
+            return hashSalt;
+
+        }
+    }
+}
